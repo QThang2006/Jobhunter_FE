adb1f6385f5c3dc3d7ce1bf6e74aabd9163cf297
QThang2006 <quocthangngo2006@gmail.com>
2026-06-21 15:17:11 +0700

    Fix routing 404 error on Vercel

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -28,8 +28,7 @@ const LoginPage = () => {
     useEffect(() => {
         //đã login => redirect to '/'
         if (isAuthenticated) {
-            // navigate('/');
-            window.location.href = '/';
+            navigate('/');
         }
     }, [])
 
@@ -43,7 +42,7 @@ const LoginPage = () => {
             localStorage.setItem('access_token', res.data.access_token);
             dispatch(setUserLoginInfo(res.data.user))
             message.success('Đăng nhập tài khoản thành công!');
-            window.location.href = callback ? callback : '/';
+            navigate(callback ? callback : '/');
         } else {
             notification.error({
                 message: "Có lỗi xảy ra",
